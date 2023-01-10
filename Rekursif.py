09f217daa0ff69cb41201c297471addbd0a0fc49
chisyanaintan <122028611+chisyanaintan@users.noreply.github.com>
2023-01-10 21:34:16 +0700

    Update Rekursif.py

--- Rekursif.py
+++ Rekursif.py
@@ -6,4 +6,10 @@ def jumlah(n: int) -> int:
 
 
 n = int(input("Masukkan nilai n : "))
+
+import datetime
+start = datetime.datetime.now()
 print("Hasil penjumlahan dari 1 hingga",n, "adalah", jumlah(n))
+end = datetime.datetime.now()
+waktu = end - start
+print("Execution Time = ", waktu.microseconds, "microseconds")
